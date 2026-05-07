b19542bf05bfa5d121cce47c7af9a80dd78288ac
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-07 15:33:55 +0300

    Created some folders and child components to arrange my work

---
+++ my-portfolio-application/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-600 text-white p-6 text-center">
+      <h1 className="text-3xl font-bold">
+        Personal Project Showcase App
+      </h1>
+    </header>
+  )
+}
+
+export default Header 
